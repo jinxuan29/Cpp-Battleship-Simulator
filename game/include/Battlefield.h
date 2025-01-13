0ac5bde35ef0ab1cd7937a24cd82734831b22028
hazy29 <jinxuanyen@gmail.com>
2025-01-13 11:16:35 +0800

    added comment for header file

--- game/include/Battlefield.h
+++ game/include/Battlefield.h
@@ -3,11 +3,13 @@
 
 class Battlefield {
 private:
-  int width;
-  int height;
-  int numberOfIsland;
-  char **grid;
-  int **islandPosition;
+  int width; // width of the baattlefield (columns)
+  int height; // height of the battlefield (row)
+  int numberOfIsland; // total number of island in the battlefield
+  char **grid; // 2d array of char to represetation the battlefield 
+  int **islandPosition;  // 2d array of all coords of the island in battlefield
+
+
   Ship ***shipGrid;
 
 public:
@@ -15,8 +17,8 @@ public:
   Battlefield(char **grid, int width, int height);
   ~Battlefield();
 
-  void display() const;
-  void placeIsland(int x, int y, char symbol);
-  void setIslandPosition();
-  void printIslandPosition();
+  void display() const; // print the battlefield
+  void placeIsland(int x, int y, char symbol); // place all the island into battlefield
+  void setIslandPosition(); // set island position into battlefield
+  void printIslandPosition(); // print all island position in (x,y) in battlefield
 };
--- game/include/GameManager.h
+++ game/include/GameManager.h
@@ -4,16 +4,22 @@
 class GameManager {
 private:
   std::string filename;
+  //Battlefiled class related 
   Battlefield
       *battlefield; // pointer for battlefield, to initialize battlefield and
                     // set the battlefield using the read file function
   char **battlefieldMap; // 2d array of the battlefield
-  int **islandPosition; // 2d array of all coords of the island in battlefield
+ 
+   // Team class related
   std::string* teamName; //aray to store the teamname for each team
-  Ship** teamShip; 
-  int interation;
-  int width;
-  int height;
+  int* teamNumShip; // array to store the number of ship each teams has. (index the same way as teamname)
+  std::string** numberOfPerShip; // 2d array to store number of each type of ship each teams has . 
+
+  // Settings 
+  int interation; // number of rounds to run the battlefield simulation
+  int width; // width of battlfield  (columns)
+  int height; // height of battlefield (rows)
+  int numberOfTeams; //total number of teams from the txt file
 
 public:
   GameManager(std::string filename);
